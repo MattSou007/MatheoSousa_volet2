50d4c4b953ebf194c4aaf0c57d20ced4b9339f4d
Matheo Sousa <90541229+MattSou007@users.noreply.github.com>
2026-03-25 11:47:03 -0400

    v1.0
    Drag n' Drop functionality has been added, basic greyboxing of gameplay

---
+++ Plumblime/Assets/scripts/Pipes.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Pipes : MonoBehaviour
+{
+    // variable générale
+    Vector3 initialPos;
+    Collider2D c2d;
+
+    // variable Drag & Drop
+    public bool beingDrag;
+    public bool isPlaced;
+    Transform intialeParent;
+
+    void Start()
+    {
+        // définir variables
+        initialPos = transform.position;
+        c2d = GetComponent<Collider2D>();
+        intialeParent = transform.parent;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    // fonctions du Drag (début action, action et fin d'action)
+    public void OnBeginDrag(BaseEventData baseEventData)
+    {
+        isPlaced=false;
+
+        // position du souris
+        PointerEventData pointerEventData = baseEventData as PointerEventData;
+        Vector3  posCursor = Camera.main.ScreenToWorldPoint(pointerEventData.position);
+        posCursor.z = 0;
+        transform.position = posCursor;
+    }
+
+    public void OnDrag(BaseEventData baseEventData)
+    {
+        
+        beingDrag = true;
+        c2d.enabled = false;
+
+        // position du souris
+        PointerEventData pointerEventData = baseEventData as PointerEventData;
+        Vector3  posCursor = Camera.main.ScreenToWorldPoint(pointerEventData.position);
+        posCursor.z = 0;
+        transform.position = posCursor;
+    }
+
+    public void OnEndDrag()
+    {
+        beingDrag = false;
+        c2d.enabled = true;
+
+        if(isPlaced==false)
+        {
+            // réinitialiser position de l'object
+            transform.position = initialPos;
+            transform.SetParent(intialeParent);
+        }
+    }
+}
---
+++ Plumblime/Assets/scripts/PlaceCases.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlaceCases : MonoBehaviour
+{
+    // variable générale
+    SpriteRenderer sr;
+    
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    // fonctions du Drop (Drop, début Hover. fin Hover)
+    public void OnDrop(BaseEventData baseEventData)
+    {
+        // obtenir l'infomation du tuyau déplacer
+        PointerEventData pointerEventData = baseEventData as PointerEventData;
+        GameObject dragObject = pointerEventData.pointerDrag;
+
+        dragObject.GetComponent<Pipes>().isPlaced = true;
+
+        // Centrer les tuyaux sur la case
+        dragObject.transform.SetParent(this.transform);
+        dragObject.transform.localPosition = Vector3.zero;
+    }
+
+    public void OnHoverBegin(BaseEventData baseEventData)
+    {
+        // obtenir l'infomation du tuyau déplacer
+        PointerEventData pointerEventData = baseEventData as PointerEventData;
+        GameObject dragObject = pointerEventData.pointerDrag;
+
+        // indication de quel cases est en sélections
+        if(dragObject!=null && dragObject.GetComponent<Pipes>().beingDrag==true)
+        {
+            sr.color = new Color(sr.color.r, sr.color.g, sr. color.b, 0.8f);
+        }
+    }
+
+    public void OnHoverEnd(BaseEventData baseEventData)
+    {
+        // obtenir l'infomation du tuyau déplacer
+        PointerEventData pointerEventData = baseEventData as PointerEventData;
+        GameObject dragObject = pointerEventData.pointerDrag;
+
+        // indication de quel cases est en sélections
+        if(dragObject!=null && dragObject.GetComponent<Pipes>().beingDrag==true)
+        {
+            sr.color = new Color(sr.color.r, sr.color.g, sr. color.b, 0f);
+        }
+    }
+}
